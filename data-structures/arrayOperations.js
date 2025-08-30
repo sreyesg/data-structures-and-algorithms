b9c5d5df63a0cfeb5c9d979296ec368920df8774
sreyeg <santiagoreyes.g@gmail.com>
2025-08-30 14:42:35 -0700

    search elements in array examples

--- data-structures/arrayOperations.js
+++ data-structures/arrayOperations.js
@@ -5,16 +5,45 @@ note: return keyword is ommitted to see all examples return values in terminal
 */
 
 function readingArrays(nums){
-    console.log(nums[3]) // access by index
-    // access all elements, linear access. For N elements, N steps. 
+    // access by index 
+    // Time Complexity: O(1)
+    console.log(nums[3]) 
+    
+    // access all elements, linear access.
+    // Time Complexity: O(n)
     nums.map((num, index)=>{
         console.log( `index: ${index} value:${num}`)
     })
     // another loop syntax with index
+    // Time Complexity: O(n)
     for(let i=0; i<nums.length; i++){
         console.log( `index: ${i}, Value:${nums[i]}`);
     }        
 
 }
+//readingArrays([1,4,5,8,4,2])
 
-readingArrays([1,4,5,8,4,2])
+//SEARCH ELEMENTS 
+
+function searchElement(nums, searchValue){
+    // loop through array
+    // Time complexity O(n)
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]=== searchValue){
+            console.log(`the searchValue is at index: ${i}`)
+        }
+    }
+    // Find index with findIndex function
+    const foundIndex = nums.findIndex(num => num === searchValue)
+    console.log(`the value searched is at ${foundIndex} index`)
+    // use find method to find element greater that 7
+    const greaterThan7 = nums.filter((num) => {
+       return num > searchValue
+    })
+    console.log(`Numbers greater than 7 are:${greaterThan7}`)
+
+    // use find method to find element greater that 7
+    const numsGreaterThan7 = nums.filter( num => num > searchValue)
+    console.log(`numbers greater than 7 are: ${numsGreaterThan7}`)
+}
+searchElement([1,4,5,8,4,2], 2)
